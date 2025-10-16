0f75f46bf90e9fdfa213e291a0762f579b0c1418
Adrian11Faudoa <afa1823@gmail.com>
2025-10-15 21:48:56 -0600

    Update Structure

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-def add_time(start, duration):
+#def add_time(start, duration):
 
 
 
 
 
-    return new_time
+#    return new_time
+
+
+
+# This entrypoint file to be used in development. Start by reading README.md
+from time_calculator import add_time
+from unittest import main
+
+
+print(add_time("11:06 PM", "2:02"))
+
+
+# Run unit tests automatically
+main(module='test_module', exit=False)
